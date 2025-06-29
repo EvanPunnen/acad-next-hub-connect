c78f30ba6210c4c52315553e0f8ed8ded57830e9
Evan Punnen <evanpunnen123@gmail.com>
2025-06-29 18:30:35 +0530

    App integration

---
+++ android/app/src/main/java/com/lovable/acadnexthub/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lovable.acadnexthub;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lovable.acadnexthub',
+  appName: 'acad-next-hub-connect',
+  webDir: 'dist'
+};
+
+export default config;
